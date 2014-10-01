5f7eb200defde843df74d9cf9339d96f0df62d68
feilste <filip.stenbeck@kambi.com>
2014-10-01 12:40:14 +0200

    added test for the hit-counter directive

--- src/demo/test/build/specs.js
+++ src/demo/test/build/specs.js
@@ -32150,7 +32150,7 @@ describe("Frontend", function() {
 		       	expect(scope.message).toEqual("No Swedish leauges found");
 		    }));
 
-		     it("should update the message when recieved data from the server", inject(function($rootScope, $controller, $httpBackend){
+		     it("should update the message when it recieves data from the server", inject(function($rootScope, $controller, $httpBackend){
 		    	var ctrl, scope = $rootScope.$new(), msg = "Allsvenskan & Division 1" ;
         		
 		    	ctrl = $controller("SwedishCtrl", { $scope: scope });
@@ -32181,6 +32181,29 @@ describe("Frontend", function() {
 		    it("should expose a method that return mock data", function(){
 		    	expect(leaugeService.getMockData()).toEqual('Allsvenskan, Division 1 Norra & Premier League');
 		    });
+		});
+	});
+
+	describe("Directives", function() {
+		describe("Hitcounter", function() {
+			var element, scope;
+
+			  it("should display page hit numbers", inject(function($rootScope, $httpBackend, $compile){
+
+			  	$httpBackend.when("GET", BACKEND_URL + "/api/counter").respond("12");
+			  	scope = $rootScope.$new();
+
+			    element = $compile('<hit-counter></hit-counter>')(scope);
+			    scope.$digest();
+				
+				//No data before server call is finished
+				expect(element[0].innerText).toBe("");
+
+			  	$httpBackend.flush();
+
+			  	//After there should be data
+			  	expect(element[0].innerText).toBe("Number of visits: 12");
+			  }));
 		});
 	});
 });
