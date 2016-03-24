910d1596301914dd274f5e7417d37e48984f6fc6
Filip Stenbeck <filip.stenbeck@svt.se>
2016-03-24 12:52:45 +0100

    upgraded loads and some refactoring

--- src/demo/server/services/football-madness.js
+++ src/demo/server/services/football-madness.js
@@ -1,20 +1,26 @@
+'use strict';
+
 module.exports = {
-	
+
 	data : require('../data/fotboll.json'),
 
 	getMessage : function (sorted, asString, unique, onlySwedish) {
-		var onlyGroupArray = [], divisions ='', found, shouldAdd;
-		for (i = 0; i < this.data.length; i++) {
+		let onlyGroupArray = [];
+		let divisions = '';
+		let found = false;
+		let shouldAdd = false;
+
+		for (let i = 0; i < this.data.length; i++) {
 			shouldAdd = false;
 			found = false;
-			if (!onlySwedish || (this.data[i].league == 'Allsvenskan' ||  
-					this.data[i].league === 'Superettan' || 
+			if (!onlySwedish || (this.data[i].league == 'Allsvenskan' ||
+					this.data[i].league === 'Superettan' ||
 					this.data[i].league === 'Division 1 Norra')
 				) {
 				shouldAdd = true;
 			}
 			if (shouldAdd) {
-				for (j = 0; j < onlyGroupArray.length; j++) {
+				for (let j = 0; j < onlyGroupArray.length; j++) {
 					if (onlyGroupArray[j] === this.data[i].league) {
 						found = true;
 						break;
